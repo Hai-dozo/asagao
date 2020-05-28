72775db593574618e0890206089a64da6e25fe01
Hai-dozo <risaishide@gmail.com>
2020-05-28 21:39:52 +0900

    -

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,33 @@
+class MembersController < ApplicationController
+    # 会員一覧
+    def index
+        @members = Member.order("number") # numberを昇順で表示
+    end
+
+    def search
+        @members = Member.search(params[:q])
+        render "index"
+    end
+
+    def show
+        @member = Member.find(params[:id])
+    end
+
+    def new
+        @member = Member.new(birthday: Date.new(1980, 1, 1)) # 誕生日の初期値を指定する
+    end
+
+    def edit
+        @member = Member.find(params[:id])
+    end
+
+    def create
+        
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
