5a71571ccbf239abde8cf5ec820b907cfb6399a4
Hai-dozo <risaishide@gmail.com>
2020-08-17 21:14:51 +0900

    p.346まで

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,27 @@ class Article < ApplicationRecord
     validates :title, :body, :released_at, presence: true
     validates :title, length: {maximum: 80}
     validates :body, length: {maximum: 2000}
+
+    def no_expiration
+        expired_at.nil?
+    end
+
+    def no_expiration=(val)
+        # 引数がtrueか1だった場合は@no_expirationをtrueにし、そうでなければfalseにする。
+        # ブラウザはフォームにチェックがついてなければ0を、ついてれば1を送ってくる。
+        @no_expiration = val.in?([true, "1"]) 
+    end
+
+    before_validation do
+        self.expired_at = nil if @no_expiration
+    end
+
+    scope :open_to_the_public, -> { where(member_only: false) }
+    
+    scope :visible, -> do
+        now = Time.current
+        where("released_at <= ?", now) # 公開日がnowかnowより前（以下）
+            .where("expired_at > ? OR expired_at IS NULL", now) # expired_atがnowより先（より大きい）
+    end
+
 end
