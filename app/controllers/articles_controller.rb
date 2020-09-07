e6d1b0e7629307cfeb0a92de9639496d7ec5fa33
Hai-dozo <risaishide@gmail.com>
2020-09-07 10:20:34 +0900

    p.349まで

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,39 @@
 class ArticlesController < ApplicationController
     before_action :login_required, except: [:index, :show]
 
+    # 記事一覧
     def index
         @articles = Article.order(released_at: :desc)
+        @articles = @articles.open_to_the_public unless current_member
+
+        unless current_member&.administrator?
+            @articles = @articles.visible
+        end
     end
 
+    # 記事詳細
     def show
-        @article = Article.find(params[:id])
+        articles = Article.all
+        articles = article.open_to_the_public unless current_member
+
+        unless current_member&.administrator?
+            articles = articles.visible
+        end
+
+        @article = articles.find(params[:id])
     end
+
 
     def new
         @article = Article.new
     end
 
+    # 記事編集
     def edit
         @article = Article.find(params[:id])
     end
 
+    # 記事作成
     def create
         @article = Article.new(params[:article])
         if @article.save
